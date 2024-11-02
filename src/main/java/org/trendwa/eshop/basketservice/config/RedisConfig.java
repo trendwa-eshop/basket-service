172cabc04e106160dae15b4391d25b3f94dcaacd
Musa BEYTEKIN <musa.beytekin1@gmail.com>
2024-11-02 13:48:22 +0300

    change workflow, check redis connection on startup

--- src/main/java/org/trendwa/eshop/basketservice/config/RedisConfig.java
+++ src/main/java/org/trendwa/eshop/basketservice/config/RedisConfig.java
@@ -1,14 +1,30 @@
 package org.trendwa.eshop.basketservice.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
+
+    @Bean
+    public ApplicationRunner redisConnectionCheck(RedisConnectionFactory redisConnectionFactory) {
+        return args -> {
+            try {
+                redisConnectionFactory.getConnection().ping();
+            } catch (RedisConnectionFailureException e) {
+                log.error("Cannot connect to Redis. Please check your Redis configuration.", e);
+                System.exit(1);
+            }
+        };
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
